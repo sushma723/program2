7bb2b5239b167b6b4dd4d82423780e4ff3ec577a
sushma723 <127485731+sushma723@users.noreply.github.com>
2023-03-10 16:09:00 +0530

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,8 @@
+package com.testingautomation;
+
+public class welcome {
+    public static void main(String[] args)
+    {
+        System.out.println("welcome to testing automaton classes");
+    }
+}
